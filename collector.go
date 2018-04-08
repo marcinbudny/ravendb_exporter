69b9252705e5d848728c6766562db654d152ff9f
Marcin Budny <marcin.budny@gmail.com>
2018-04-08 19:56:34 +0200

    some refactoring

---
+++ collector.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"github.com/prometheus/client_golang/prometheus"
+)
+
+const (
+	namespace = "ravendb"
+	subsystem = "exporter"
+)
+
+type exporter struct {
+	up         prometheus.Gauge
+	workingSet prometheus.Gauge
+}
+
+func newExporter() *exporter {
+	return &exporter{
+		up:         createGauge("up", "Whether the RavenDB scrape was successful"),
+		workingSet: createGauge("ravendb_working_set_bytes", "Process working set"),
+	}
+}
+
+func (e *exporter) Describe(ch chan<- *prometheus.Desc) {
+	ch <- e.up.Desc()
+	ch <- e.workingSet.Desc()
+}
+
+func (e *exporter) Collect(ch chan<- prometheus.Metric) {
+	log.Info("Running scrape")
+
+	if stats, err := getStats(); err != nil {
+		log.WithError(err).Error("Error while getting data from RavenDB")
+
+		e.up.Set(0)
+		ch <- e.up
+	} else {
+		e.up.Set(1)
+		ch <- e.up
+
+		e.workingSet.Set(stats.memory["WorkingSet"].(float64))
+		ch <- e.workingSet
+	}
+}
+
+func createGauge(name string, help string) prometheus.Gauge {
+	return prometheus.NewGauge(prometheus.GaugeOpts{
+		Namespace: namespace,
+		Subsystem: subsystem,
+		Name:      name,
+		Help:      help,
+	})
+}
--- ravendb_exporter.go
+++ ravendb_exporter.go
@@ -13,11 +13,6 @@ import (
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
-const (
-	namespace = "ravendb"
-	subsystem = "exporter"
-)
-
 var (
 	log = logrus.New()
 
@@ -31,49 +26,6 @@ var (
 	clientCertFile string
 	clientKeyFile  string
 )
-
-type exporter struct {
-	up         prometheus.Gauge
-	workingSet prometheus.Gauge
-}
-
-func newExporter() *exporter {
-	return &exporter{
-		up:         createGauge("up", "Whether the RavenDB scrape was successful"),
-		workingSet: createGauge("ravendb_working_set_bytes", "Process working set"),
-	}
-}
-
-func (e *exporter) Describe(ch chan<- *prometheus.Desc) {
-	ch <- e.up.Desc()
-	ch <- e.workingSet.Desc()
-}
-
-func (e *exporter) Collect(ch chan<- prometheus.Metric) {
-	log.Info("Running scrape")
-
-	if stats, err := getStats(); err != nil {
-		log.WithError(err).Error("Error while getting data from RavenDB")
-
-		e.up.Set(0)
-		ch <- e.up
-	} else {
-		e.up.Set(1)
-		ch <- e.up
-
-		e.workingSet.Set(stats.memory["WorkingSet"].(float64))
-		ch <- e.workingSet
-	}
-}
-
-func createGauge(name string, help string) prometheus.Gauge {
-	return prometheus.NewGauge(prometheus.GaugeOpts{
-		Namespace: namespace,
-		Subsystem: subsystem,
-		Name:      name,
-		Help:      help,
-	})
-}
 
 func serveLandingPage() {
 	var landingPage = []byte(`<html>
@@ -91,8 +43,7 @@ func serveLandingPage() {
 }
 
 func serveMetrics() {
-	exporter := newExporter()
-	prometheus.MustRegister(exporter)
+	prometheus.MustRegister(newExporter())
 
 	http.Handle("/metrics", promhttp.Handler())
 }
