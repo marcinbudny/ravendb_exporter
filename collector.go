0497ba1ec94aadb5aa78e9cf691c5f0932958228
Marcin Budny <marcin.budny@gmail.com>
2018-04-29 21:49:12 +0200

    more metrics

--- collector.go
+++ collector.go
@@ -1,24 +1,43 @@
 package main
 
 import (
+	"strconv"
+	"regexp"
 	"github.com/prometheus/client_golang/prometheus"
 	jp "github.com/buger/jsonparser"
 )
 
 const (
 	namespace = "ravendb"
-	subsystem = "exporter"
+	subsystem = ""
 )
 
 type exporter struct {
-	up         prometheus.Gauge
-	workingSet prometheus.Gauge
+	up         					prometheus.Gauge
+	workingSet 					prometheus.Gauge
+	cpuTime						prometheus.Counter
+	isLeader					prometheus.Gauge
+	requestCount				prometheus.Counter
+	documentPutCount			prometheus.Counter
+	documentPutBytes			prometheus.Counter
+	mapIndexIndexedCount		prometheus.Counter
+	mapReduceIndexMappedCount	prometheus.Counter
+	mapReduceIndexReducedCount	prometheus.Counter
+
 }
 
 func newExporter() *exporter {
 	return &exporter{
-		up:         createGauge("up", "Whether the RavenDB scrape was successful"),
-		workingSet: createGauge("ravendb_working_set_bytes", "Process working set"),
+		up:         		createGauge("up", "Whether the RavenDB scrape was successful"),
+		workingSet: 		createGauge("working_set_bytes", "Process working set"),
+		cpuTime:			createCounter("cpu_time_seconds", "CPU time"),
+		isLeader:			createGauge("is_leader", "If 1, then node is the cluster leader, otherwise 0"),
+		requestCount:		createCounter("request_count", "Server-wide request count"),
+		documentPutCount:	createCounter("document_put_count", "Server-wide document puts count"),
+		documentPutBytes:	createCounter("document_put_bytes", "Server-wide document put bytes"),
+		mapIndexIndexedCount:	createCounter("mapindex_indexed_count", "Server-wide map index indexed count"),
+		mapReduceIndexMappedCount:	createCounter("mapreduceindex_mapped_count", "Server-wide map-reduce index mapped count"),
+		mapReduceIndexReducedCount:	createCounter("mapreduceindex_reduced_count", "Server-wide map-reduce index reduced count"),
 	}
 }
 
@@ -39,10 +58,85 @@ func (e *exporter) Collect(ch chan<- prometheus.Metric) {
 		e.up.Set(1)
 		ch <- e.up
 
-		value, _ := jp.GetFloat(stats.memory, "WorkingSet")
-		e.workingSet.Set(value)
+		e.workingSet.Set(getMemoryWorkingSet(stats))
 		ch <- e.workingSet
+
+		e.cpuTime.Set(getCPUTime(stats))
+		ch <- e.cpuTime
+
+		e.isLeader.Set(getIsLeader(stats))
+		ch <- e.isLeader
+
+		e.requestCount.Set(getRequestCount(stats))
+		ch <- e.requestCount
+
+		e.documentPutCount.Set(getDocumentPutCount(stats))
+		ch <- e.documentPutCount
+
+		e.documentPutBytes.Set(getDocumentPutBytesCount(stats))
+		ch <- e.documentPutBytes
+
+		e.mapIndexIndexedCount.Set(getMapIndexIndexedCount(stats))
+		ch <- e.mapIndexIndexedCount
+
+		e.mapReduceIndexMappedCount.Set(getMapReduceIndexMappedCount(stats))
+		ch <- e.mapReduceIndexMappedCount
+
+		e.mapReduceIndexReducedCount.Set(getMapReduceIndexReducedCount(stats))
+		ch <- e.mapReduceIndexReducedCount
 	}
+}
+
+func getCPUTime(stats *stats) float64 {
+	var cpuTimeString string
+	jp.ArrayEach(stats.cpu, func(value []byte, dataType jp.ValueType, offset int, err error) {
+		cpuTimeString, _ = jp.GetString(value, "TotalProcessorTime") // just use the last entry in the array TODO: why is this an array?
+	}, "CpuStats")
+
+	return timeSpanToSeconds(cpuTimeString)
+}
+
+func getMemoryWorkingSet(stats *stats) float64 {
+	value, _ := jp.GetFloat(stats.memory, "WorkingSet")
+	return value
+}
+
+func getIsLeader(stats * stats) float64 {
+	value, _ := jp.GetString(stats.nodeInfo, "CurrentState")
+	if value == "Leader" {
+		return 1
+	} 
+	return 0
+}
+
+func getRequestCount(stats *stats) float64 {
+	value, _ := jp.GetFloat(stats.metrics, "Requests", "RequestsPerSec", "Count")
+	return value
+}
+
+func getDocumentPutCount(stats *stats) float64 {
+	value, _ := jp.GetFloat(stats.metrics, "Docs", "PutsPerSec", "Count")
+	return value
+}
+
+func getDocumentPutBytesCount(stats *stats) float64 {
+	value, _ := jp.GetFloat(stats.metrics, "Docs", "BytesPutsPerSec", "Count")
+	return value
+}
+
+func getMapIndexIndexedCount(stats *stats) float64 {
+	value, _ := jp.GetFloat(stats.metrics, "MapIndexes", "MappedPerSec", "Count")
+	return value
+}
+
+func getMapReduceIndexMappedCount(stats *stats) float64 {
+	value, _ := jp.GetFloat(stats.metrics, "MapReduceIndexes", "MappedPerSec", "Count")
+	return value
+}
+
+func getMapReduceIndexReducedCount(stats *stats) float64 {
+	value, _ := jp.GetFloat(stats.metrics, "MapReduceIndexes", "ReducedPerSec", "Count")
+	return value
 }
 
 func createGauge(name string, help string) prometheus.Gauge {
@@ -52,4 +146,56 @@ func createGauge(name string, help string) prometheus.Gauge {
 		Name:      name,
 		Help:      help,
 	})
+}
+
+func createCounter(name string, help string) prometheus.Counter {
+	return prometheus.NewCounter(prometheus.CounterOpts {
+		Namespace: 	namespace,
+		Subsystem: 	subsystem,
+		Name:		name,
+		Help:		help,
+	})
+}
+
+var timespanRegex = regexp.MustCompile(`((?P<days>\d+)\.)?(?P<hours>\d{2}):(?P<minutes>\d{2}):(?P<seconds>\d{2})(\.(?P<secondfraction>\d{7}))?`)
+
+func timeSpanToSeconds(timespanString string) float64 {
+	
+	var result float64
+
+	matches := matchNamedGroups(timespanRegex, timespanString)
+	if daysString, ok := matches["days"]; ok {
+		days, _ := strconv.Atoi(daysString)
+		result = result + float64(days) * 24 * 60 * 60
+	}
+	if hoursString, ok := matches["hours"]; ok {
+		hours, _ := strconv.Atoi(hoursString)
+		result = result + float64(hours) * 60 * 60
+	}
+	if minutesString, ok := matches["minutes"]; ok {
+		minutes, _ := strconv.Atoi(minutesString)
+		result = result + float64(minutes) * 60
+	}
+	if secondsString, ok := matches["seconds"]; ok {
+		seconds, _ := strconv.Atoi(secondsString)
+		result = result + float64(seconds) 
+	}
+	if secondFractionString, ok := matches["secondfraction"]; ok {
+		secondFraction, _ := strconv.Atoi(secondFractionString)
+		result = result + float64(secondFraction) / 10000000
+	}
+
+	return result
+}
+
+func matchNamedGroups(regex *regexp.Regexp, text string) map[string]string {
+	matches := regex.FindStringSubmatch(text)
+
+	results := make(map[string]string)
+	for i, name := range regex.SubexpNames() {
+		if name != "" {
+			results[name] = matches[i]
+		}
+	}
+	return results
 }
---
+++ collector_test.go
@@ -0,0 +1,20 @@
+package main
+
+import "testing"
+
+func TestParseTimeSpan(t *testing.T) {
+	
+	testCases := make(map[string]float64)
+	testCases["55.11:22:33.0444000"] = 55 * 24 * 60 * 60 + 11 * 60 * 60 + 22 * 60 + 33 + 0.0444000
+	testCases["55.11:22:33"] = 55 * 24 * 60 * 60 + 11 * 60 * 60 + 22 * 60 + 33
+	testCases["11:22:33"] = 11 * 60 * 60 + 22 * 60 + 33
+	
+	for testCase, expected := range testCases {
+		t.Run(testCase, func(t *testing.T) {
+			actual := timeSpanToSeconds(testCase)
+			if actual != expected {
+				t.Errorf("Timespan string %s should parse to %fs but parsed to %fs", testCase, expected, actual)
+			}
+		})
+	}
+}
